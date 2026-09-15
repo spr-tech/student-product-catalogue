ea5e4b9f70d37e5296a75ccc2b4718c41d2e40f0
spr-tech <adetoroshina434@gmail.com>
2026-09-15 16:22:01 +0100

    add routes , and add route pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return <h1>Between Classes</h1>;
 }
---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductDetail = () => {
+  return (
+    <div>
+      This is the product detail page
+      
+    </div>
+  )
+}
+
+export default ProductDetail
